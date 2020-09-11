53b3275115f7797f3f065d37d9ebf560fa2abd69
park-daeun <70877497+park-daeun@users.noreply.github.com>
2020-09-11 23:22:40 +0900

    Create Q1008.java
    백준 #1008 자바 소스코드

---
+++ 1000~1999/Q1008.java
@@ -0,0 +1,18 @@
+//Backjoon step1 - A/B
+
+package Baekjoon1;
+
+import java.util.Scanner;
+
+public class Q1008 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		double a,b;
+		
+		a = input.nextInt();
+		b = input.nextInt();
+		
+		System.out.println(a/b);
+	}
+}
