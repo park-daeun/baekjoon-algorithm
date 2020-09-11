f32501c5b42a77872f73517ad7f9dcccc9cee2d5
park-daeun <70877497+park-daeun@users.noreply.github.com>
2020-09-11 23:21:38 +0900

    Create Q10172.java
    백준 #10172 자바 소스코드

---
+++ 10000~10999/Q10172.java
@@ -0,0 +1,9 @@
+//Backjoon step1 - °³
+
+package Baekjoon1;
+
+public class Q10172 {
+	public static void main(String arg[]) {
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+	}
+}
