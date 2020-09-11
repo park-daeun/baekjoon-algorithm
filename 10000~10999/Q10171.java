b3792657c7eccd964704854cffd4a883646cea58
park-daeun <70877497+park-daeun@users.noreply.github.com>
2020-09-11 23:21:29 +0900

    Create Q10171.java
    백준 #10171 자바 소스코드

---
+++ 10000~10999/Q10171.java
@@ -0,0 +1,9 @@
+//Backjoon step1 - °í¾çÀÌ
+
+package Baekjoon1;
+
+public class Q10171 {
+	public static void main(String args[]) {
+		System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|\n");
+	}
+}
