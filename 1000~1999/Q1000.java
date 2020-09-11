6d2dd66f39b88b7cdf504087760df1d7859e4728
park-daeun <70877497+park-daeun@users.noreply.github.com>
2020-09-11 23:20:37 +0900

    Create Q1000.java
      백준 #1000 A+B 자바 소스코드

---
+++ 1000~1999/Q1000.java
@@ -0,0 +1,18 @@
+//Backjoon step1 - A+B
+
+package Baekjoon1;
+
+import java.util.Scanner;
+
+public class Q1000 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		int a,b;
+		
+		a = input.nextInt();
+		b = input.nextInt();
+		
+		System.out.println(a+b);
+	}
+}
