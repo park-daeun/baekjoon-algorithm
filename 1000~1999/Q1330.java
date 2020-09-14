a848c600cd458521e5bc82c2450040272c20bb1d
park-daeun <70877497+park-daeun@users.noreply.github.com>
2020-09-15 02:19:15 +0900

    두 수 비교하기

---
+++ 1000~1999/Q1330.java
@@ -0,0 +1,21 @@
+package Baekjoon2;
+
+import java.util.Scanner;
+
+public class Q1330 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a,b;
+		a = input.nextInt();
+		b = input.nextInt();
+		
+		if(a>b)
+			System.out.println(">");
+		else if(a<b)
+			System.out.println("<");
+		else
+			System.out.println("==");
+			
+	}
+}
