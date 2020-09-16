344ea84180cba676c6cb7175c81b5008c2713b42
park-daeun <70877497+park-daeun@users.noreply.github.com>
2020-09-17 01:48:31 +0900

    N 찍기

---
+++ 2000~2999/Q2741.java
@@ -0,0 +1,15 @@
+package Baekjoon3;
+
+import java.util.Scanner;
+
+public class Q2741 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println(i);
+		}
+	}
+}
