3482139c78bd904fc858e656bf513a874e983dcc
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-01-28 23:30:01 +0900

    곱셈

---
+++ 2000~2999/Q2588.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+A = int(input())
+B = list(input())
+print(A*int(B[2]))
+print(A*int(B[1]))
+print(A*int(B[0]))
+print(A*int(B[2])+A*int(B[1])*10+A*int(B[0])*100)
+
