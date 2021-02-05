3d6f8b68484d049ca49625266146b838fda260ff
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-02-05 23:40:20 +0900

    알람 시계

---
+++ 2000~2999/Q2884.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+h,m = map(int, input().split())
+
+if m >= 45:
+    m = m-45
+else:
+    m = 60 - (45-m)
+    if h == 0:
+        h = 23
+    else:
+        h = h-1
+print(h,m)
+
