dea604f4f17e89d31066329877d0eae7aea62721
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-08-17 04:13:10 +0900

    랜선 자르기

---
+++ 1000~1999/Q1654.py
@@ -0,0 +1,36 @@
+import sys
+
+def binary_search(arr, target, start, end):
+    ans = 0
+    while start <= end:
+        mid = (start + end) // 2
+
+        cnt = 0
+        for a in arr:
+            cnt += a // mid
+        print(mid, cnt)
+        
+        if cnt >= target:
+            ans = max(mid, ans)
+            start = mid + 1
+        else:
+            end = mid - 1
+    
+    return ans
+
+k, n = map(int, sys.stdin.readline().split())
+arr = [int(sys.stdin.readline()) for _ in range(k)]
+
+print(binary_search(arr, n, 1, max(arr)))
+
+'''
+입력 예시
+4 11
+802
+743
+457
+539
+
+출력 예시
+200
+'''
