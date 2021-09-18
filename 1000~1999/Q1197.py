901e986653b83600db77ef4bf6723792ea896d24
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-09-18 23:25:13 +0900

    Create Q1197.py

---
+++ 1000~1999/Q1197.py
@@ -0,0 +1,49 @@
+import sys
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, sys.stdin.readline().split())
+parent = [0] * (v+1)
+
+for i in range(1, v+1):
+    parent[i] = i
+
+edges = []
+result = 0
+
+for _ in range(e):
+    a, b, cost = map(int, sys.stdin.readline().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
+
+'''
+입력 예시
+3 3
+1 2 1
+2 3 2
+1 3 3
+
+출력 예시
+3
+'''
