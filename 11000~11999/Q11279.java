c9684293901aacbfccf0d6869c63a2f288e3c970
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-09-23 16:47:07 +0900

    최대 힙

---
+++ 11000~11999/Q11279.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Q11279 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0; i<n; i++){
+            int x = Integer.parseInt(br.readLine());
+
+            if(x == 0){
+                if(pQueue.isEmpty()){
+                    bw.write(0 + "\n");
+                }
+                else{
+                    bw.write(pQueue.poll() + "\n");
+                }
+            }
+            else{
+                pQueue.add(x);
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
