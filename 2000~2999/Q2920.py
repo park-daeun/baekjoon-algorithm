cee8c8d85046c666c08de2b29c0396d7616e524e
park-daeun <70877497+park-daeun@users.noreply.github.com>
2021-11-12 09:13:57 +0900

    음계

---
+++ 2000~2999/Q2920.py
@@ -0,0 +1,10 @@
+import sys
+
+s = sys.stdin.readline().rstrip()
+
+if s == "1 2 3 4 5 6 7 8":
+    print("ascending")
+elif s == "8 7 6 5 4 3 2 1":
+    print("descending")
+else:
+    print("mixed")
